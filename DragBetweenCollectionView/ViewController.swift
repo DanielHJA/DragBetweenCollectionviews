970cd746a3163f67430938fb32e72c10195f6cf1
DanielHJA <daniel.hjartstrom@gmail.com>
2019-04-09 08:33:25 +0200

    Initial Commit

---
+++ DragBetweenCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DragBetweenCollectionView
+//
+//  Created by Daniel Hjärtström on 2019-04-09.
+//  Copyright © 2019 Sog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
